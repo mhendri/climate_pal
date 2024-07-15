04465df80ef14ff2d986037170da4ece33fb2061
Behrad Rabiei <brabiei@ucsd.edu>
2024-07-15 12:29:18 -0700

    deleted key.py

--- langchain_demo/key.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# How do I hide this from git commits?
-def init():
-    os.environ["OPENAI_API_KEY"] = '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' # Put your key here!
-
-# import dotenv
-
-# dotenv.load_dotenv()
